64cf6e09cfa854f9f01926cd7eb244d6578bce0e
Jake Kim <jake@getawair.com>
2022-10-17 13:19:41 +0900

    first attempt 8queens

--- hw3/8queens.py
+++ hw3/8queens.py
@@ -1,33 +1,39 @@
 import random
+import time
 
 POPULATION_SIZE = 5		# 개체 집단의 크기
 MUTATION_RATE = 0.1			# 돌연 변이 확률
-SIZE = 5				# 하나의 염색체에서 유전자 개수		
+SIZE = 8				# 하나의 염색체에서 유전자 개수
 
-# 염색체를 클래스로 정의한다. 
+def gen_queen():
+    return random.randrange(8)
+
+# 염색체를 클래스로 정의한다.
 class Chromosome:
     def __init__(self, g=[]):
-        self.genes = g.copy()		# 유전자는 리스트로 구현된다. 
+        self.genes = g.copy()		# 유전자는 리스트로 구현된다.
         self.fitness = 0		# 적합도
-        if self.genes.__len__()==0:	# 염색체가 초기 상태이면 초기화한다. 
+        if self.genes.__len__()==0:	# 염색체가 초기 상태이면 초기화한다.
             i = 0
             while i<SIZE:
-                if random.random() >= 0.5: self.genes.append(1)
-                else: self.genes.append(0)
+                self.genes.append(gen_queen())
                 i += 1
-    
-    def cal_fitness(self):		# 적합도를 계산한다. 
+
+    def cal_fitness(self):		# 적합도를 계산한다.
         self.fitness = 0;
-        value = 0
+        value = 28
         for i in range(SIZE):
-            value += self.genes[i]*pow(2,SIZE-1-i)
+            for j in range (i+1, SIZE):
+                if (self.genes[i] == self.genes[j] or
+                    abs(self.genes[i] - self.genes[j]) == abs(i - j)):
+                    value -= 1
         self.fitness = value
         return self.fitness
 
     def __str__(self):
         return self.genes.__str__()
 
-# 염색체와 적합도를 출력한다. 
+# 염색체와 적합도를 출력한다.
 def print_p(pop):
     i = 0
     for x in pop:
@@ -37,10 +43,10 @@ def print_p(pop):
 
 # 선택 연산
 def select(pop):
-    max_value  = sum([c.cal_fitness() for c in population])
+    max_value  = sum([c.cal_fitness() for c in pop])
     pick    = random.uniform(0, max_value)
     current = 0
-    
+
     # 룰렛휠에서 어떤 조각에 속하는지를 알아내는 루프
     for c in pop:
         current += c.cal_fitness()
@@ -52,24 +58,21 @@ def crossover(pop):
     father = select(pop)
     mother = select(pop)
     index = random.randint(1, SIZE - 2)
-    child1 = father.genes[:index] + mother.genes[index:] 
-    child2 = mother.genes[:index] + father.genes[index:] 
+    child1 = father.genes[:index] + mother.genes[index:]
+    child2 = mother.genes[:index] + father.genes[index:]
     return (child1, child2)
-    
+
 # 돌연변이 연산
 def mutate(c):
     for i in range(SIZE):
         if random.random() < MUTATION_RATE:
-            if random.random() < 0.5:
-                c.genes[i] = 1
-            else:
-                c.genes[i] = 0
+            c.genes[i] = gen_queen()
 
 # 메인 프로그램
 population = []
 i=0
 
-# 초기 염색체를 생성하여 객체 집단에 추가한다. 
+# 초기 염색체를 생성하여 객체 집단에 추가한다.
 while i<POPULATION_SIZE:
     population.append(Chromosome())
     i += 1
@@ -80,7 +83,7 @@ print("세대 번호=", count)
 print_p(population)
 count=1
 
-while population[0].cal_fitness() < 31:
+while population[0].cal_fitness() < 28:
     new_pop = []
 
     # 선택과 교차 연산
@@ -89,10 +92,10 @@ while population[0].cal_fitness() < 31:
         new_pop.append(Chromosome(c1));
         new_pop.append(Chromosome(c2));
 
-    # 자식 세대가 부모 세대를 대체한다. 
-    # 깊은 복사를 수행한다. 
-    population = new_pop.copy();    
-    
+    # 자식 세대가 부모 세대를 대체한다.
+    # 깊은 복사를 수행한다.
+    population = new_pop.copy();
+
     # 돌연변이 연산
     for c in population: mutate(c)
 
@@ -101,4 +104,4 @@ while population[0].cal_fitness() < 31:
     print("세대 번호=", count)
     print_p(population)
     count += 1
-    if count > 100 : break;
+    if count > 1000 : break;
