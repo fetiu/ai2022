9143516465f601a0902a60da7a5ace9f0af78f88
Jake Kim <jake@getawair.com>
2022-12-01 11:35:51 +0900

    working csv test

---
+++ hw4/csv_test.py
@@ -0,0 +1,16 @@
+from csv import DictReader
+
+with open('metro-toilets.csv', 'r', encoding='cp949') as f:
+    data = DictReader(f);
+    for row in data:
+        print(f'{row["화장실명"]} {row["위도"]} {row["경도"]}')
+
+with open('gyeonggi-toilets.csv', 'r', encoding='cp949') as f:
+    data = DictReader(f);
+    for row in data:
+        print(f'{row["화장실명"]} {row["위도"]} {row["경도"]}')
+
+with open('seoul-toilets2.csv', 'r') as f:
+    data = DictReader(f);
+    for row in data:
+        print(f'{row["화장실명"]} {row["위도"]} {row["경도"]}')
